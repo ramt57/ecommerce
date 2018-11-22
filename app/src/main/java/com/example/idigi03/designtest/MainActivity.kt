614294a410f0b8c67029c8308e898d25a1cef888
ramt57 <ramt57@gmail.com>
2018-11-22 16:36:14 +0530

    push

--- app/src/main/java/com/example/idigi03/designtest/MainActivity.kt
+++ app/src/main/java/com/example/idigi03/designtest/MainActivity.kt
@@ -2,13 +2,20 @@ package com.example.idigi03.designtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_appbar.replaceMenu(R.menu.menu_bottomappbar)
+        var view=findViewById<View>(R.id.bottomsheet)
+        var behavior=BottomSheetBehavior.from(view)
+
+        fab.setOnClickListener {
+            behavior.state=BottomSheetBehavior.STATE_EXPANDED
+        }
     }
 }
